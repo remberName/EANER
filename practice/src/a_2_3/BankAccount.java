03b7566415087f8ea6777379e1210225388a762f
remberName <share@192.168.3.10>
2025-02-20 10:57:05 +0900

    A-2-3演習課題完成

---
+++ practice/src/a_2_3/BankAccount.java
@@ -0,0 +1,50 @@
+package a_2_3;
+
+import java.math.BigDecimal;
+
+/**
+ * [概要] <p>銀行口座クラス。</p>
+ * [説明] <p>銀行口座クラス。</p>
+ * [補充] <p>特になし。</p>
+ */
+public class BankAccount {
+	
+	/**
+	 * 残高
+	 * デフォルト値：0.00
+	 */
+	private Double balance = 0.00;
+	
+	public BankAccount(Double balance) {
+		super();
+		this.balance = balance;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+	
+	/**
+	 * [概要] <p>引き出しメソッド。</p>
+	 * [説明] <p>引き出しメソッド。</p>
+	 * [補充] <p>特になし。</p>
+	 *
+	 * @param amount 引き出し金額
+	 * @return　引き出した後残高
+	 */
+	public void withdraw(int amount) {
+
+		// 残高より多い場合
+		if (amount > balance) {
+			System.out.println("残高不足です。");
+		} else {
+			// 残高より少ない場合
+			this.balance = this.balance - amount;
+			
+			// 数字大きすぎるため、BigDecimalに転換する
+			BigDecimal bigDecimal = new BigDecimal(balance);
+			// 引き出した後残高
+			System.out.println("残高：" +bigDecimal.toPlainString());
+		}
+	}
+}
