b78e730c798ce787ad4aa6bfc830a36c727fef1e
remberName <share@192.168.3.10>
2025-02-21 17:56:10 +0900

    A-2-5スレッドを完成しました

---
+++ practice/src/a_2_5/Index.java
@@ -0,0 +1,29 @@
+package a_2_5;
+
+import java.util.concurrent.ExecutionException;
+
+/**
+ * [概要] <p>入口クラス。</p>
+ * [説明] <p>入口クラス。</p>
+ * [補充] <p>特になし。</p>
+ */
+public class Index {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		// TODO Auto-generated method stub
+
+		// 二つスレッド交互クラス
+		TowThread towThread = new TowThread();
+
+		towThread.towThread();
+
+		// スレッドプール練習クラス
+		ThreadPoolPractise poolPractise = new ThreadPoolPractise();
+		// 非同期実行
+		poolPractise.threadPoolPractiseAsynchronous();
+
+		// 同期実行
+		poolPractise.threadPoolPractiseBlocking();
+	}
+
+}
